4122cc4847d1c6f160a72863480cdfede131d4d6
udayrajput1211 <udayrajput1211@gmail.com>
2026-03-07 00:14:39 +0530

    Rename parameter in orderProduct function

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-function orderProduct(name){
+function orderProduct(product){
 
-document.getElementById("productName").value = name;
+document.getElementById("productName").value = product;
 
-window.location.href="#payment";
+document.getElementById("checkout").scrollIntoView({
+behavior:"smooth"
+})
+
+}
+
+
+function goToPayment(){
+
+let name=document.getElementById("name").value
+
+let product=document.getElementById("productName").value
+
+document.getElementById("finalName").value=name
+
+document.getElementById("finalProduct").value=product
+
+document.getElementById("payment").style.display="block"
+
+document.getElementById("payment").scrollIntoView({
+behavior:"smooth"
+})
 
 }
